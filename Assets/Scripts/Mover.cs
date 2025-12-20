3d9a959136a0948b81ae834d301b4db8c2355550
FranzCantPickAName <soheil.rasouly@gmail.com>
2025-12-20 11:54:36 +0330

    Mover progress

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-
-    [SerializeField] float xValue = 0f;
-    [SerializeField] float yValue = 0.05f;
-    [SerializeField] float zValue = 0f;
+    [SerializeField] float moveSpeed = 10f;
+    //[SerializeField] float xValue = 0f;
+    //[SerializeField] float yValue = 0.05f;
+    //[SerializeField] float zValue = 0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,6 +16,9 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float yValue = 0f;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         //transform.Translate(0.01f, 0f, 0f);
         transform.Translate(xValue, yValue, zValue);
     }
